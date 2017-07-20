c8b81998eb49f48066838621342ac2b85c3a7eed
K K <kalkotekedar@gmail.com>
2017-07-20 02:56:10 -0700

    New project created for XamrinExamples

---
+++ XamarinExamples/XamarinExamples/ExamplesHome.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinExamples
+{
+	public partial class ExamplesHome : ContentPage
+	{
+		public ExamplesHome()
+		{
+			InitializeComponent();
+		}
+	}
+}
