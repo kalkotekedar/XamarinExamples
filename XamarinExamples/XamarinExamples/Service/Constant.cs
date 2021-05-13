37b94334cc26338907ffc8ce0056f872a8edb60c
K K <kalkotekedar@gmail.com>
2021-05-14 00:11:56 +0530

    Update Constant.cs

--- XamarinExamples/XamarinExamples/Service/Constant.cs
+++ XamarinExamples/XamarinExamples/Service/Constant.cs
@@ -7,6 +7,6 @@ namespace XamarinExamples
     class Constant
     {
         public static string BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
-        public static string API_KEY = "cdd937935d564684393e5712d1d7e364";
+        public static string API_KEY = "{Weather API Key}";
     }
 }
